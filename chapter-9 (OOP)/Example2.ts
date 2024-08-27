cfe2abb4f51d5d862915ec99a959587021df6647
Pranay <pranayramteke613@gmail.com>
2024-08-27 01:35:54 -0700

    Added new code files

---
+++ chapter-9 (OOP)/Example2.ts
@@ -0,0 +1,13 @@
+
+class Rectangle{
+    private height:number;
+    private width:number;
+
+    public constructor(height:number, width:number)
+    {
+        this.height = height;
+        this.width = width;
+    }
+
+
+}
